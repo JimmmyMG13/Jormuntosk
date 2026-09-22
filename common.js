84608d08c272c3efdef4d2e06cfc43c7cac80af3
JimmmyMG13 <jimmymg13@gmail.com>
2026-09-22 21:22:54 +0200

    Add files via upload

--- common.js
+++ common.js
@@ -538,6 +538,7 @@ export const COL_RUNENMAUER_HIGHSCORES = "runenmauer_highscores";
 export const COL_MIDGARDSCHLANGE_HIGHSCORES = "midgardschlange_highscores";
 export const COL_RUNENVERSCHMELZUNG_HIGHSCORES = "runenverschmelzung_highscores";
 export const COL_DRACHENFLUG_HIGHSCORES = "drachenflug_highscores";
+export const COL_FAFNIRSHORT_HIGHSCORES = "fafnirshort_highscores";
 
 export const ANFRAGE_KATEGORIEN = ["Ausrüstung", "Finanzen", "Organisatorisches", "Sonstiges"];
 
